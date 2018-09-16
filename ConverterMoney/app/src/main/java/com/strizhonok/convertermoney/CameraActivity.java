d69b2d7e281ebda908b1873a68debe2ff39a9c5e
Strizhonok <strizhonok15@gmail.com>
2018-09-16 22:41:10 +0300

    work!

--- ConverterMoney/app/src/main/java/com/strizhonok/convertermoney/CameraActivity.java
+++ ConverterMoney/app/src/main/java/com/strizhonok/convertermoney/CameraActivity.java
@@ -2,28 +2,18 @@ package com.strizhonok.convertermoney;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
-import java.io.IOException;
-
 public class CameraActivity extends AppCompatActivity
 {
-    final int CAMERA_ID = 0;
     final int REQUEST_CODE_PERMISSION_READ_CONTACTS = 1;
-
-    SurfaceView surfaceView;
-    SurfaceHolder holder;
-    Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,11 +22,6 @@ public class CameraActivity extends AppCompatActivity
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_camera);
-
-        surfaceView = findViewById(R.id.cameraView);
-        holder = surfaceView.getHolder();
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
         {
@@ -45,7 +30,7 @@ public class CameraActivity extends AppCompatActivity
         }
         else
         {
-            Toast.makeText(this, "нененееннене", Toast.LENGTH_LONG).show();
+            startCamera();
         }
     }
 
@@ -57,7 +42,6 @@ public class CameraActivity extends AppCompatActivity
             case REQUEST_CODE_PERMISSION_READ_CONTACTS:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 {
-                    Toast.makeText(this, R.string.premission, Toast.LENGTH_LONG).show();
                     startCamera();
                 }
                 else
@@ -65,42 +49,11 @@ public class CameraActivity extends AppCompatActivity
                     Toast.makeText(this, R.string.not_premission, Toast.LENGTH_LONG).show();
                 }
         }
-        return;
     }
 
     private void startCamera()
     {
-        camera = Camera.open(CAMERA_ID);
-
-        holder.addCallback(new HolderCallback());
-    }
-
-    class HolderCallback implements SurfaceHolder.Callback {
-        @Override
-        public void surfaceCreated(SurfaceHolder holder) {
-            try {
-                camera.setPreviewDisplay(holder);
-                camera.startPreview();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                                   int height) {
-            camera.stopPreview();
-            //setCameraDisplayOrientation(CAMERA_ID);
-            try {
-                camera.setPreviewDisplay(holder);
-                camera.startPreview();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void surfaceDestroyed(SurfaceHolder holder) {
-        }
+        HolderCallback preview = new HolderCallback(this);
+        setContentView(preview);
     }
 }
