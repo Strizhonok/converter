47af4c99554d3ebc1221da20c68f5076532ba20d
Strizhonok <strizhonok15@gmail.com>
2018-11-08 19:38:27 +0300

    test2

--- ConverterMoney/app/src/main/java/com/strizhonok/convertermoney/CameraActivity.java
+++ ConverterMoney/app/src/main/java/com/strizhonok/convertermoney/CameraActivity.java
@@ -1,19 +1,72 @@
 package com.strizhonok.convertermoney;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Display;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class CameraActivity extends AppCompatActivity
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+public class CameraActivity extends AppCompatActivity implements SurfaceHolder.Callback, Camera.AutoFocusCallback, Camera.PictureCallback
 {
-    final int REQUEST_CODE_PERMISSION_READ_CONTACTS = 1;
+    final int REQUEST_CODE_PERMISSION_CAMERA = 1;
+    final int CAMERA_ID = 0;
+
+    protected SurfaceHolder holder;
+    protected SurfaceView surfaceView;
+    protected Camera camera;
+    protected LinearLayout cameraLayout;
+   // protected FirebaseAnalytics mFirebaseAnalytics;
+
+   /* private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static
+    {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,10 +76,15 @@ public class CameraActivity extends AppCompatActivity
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        setContentView(R.layout.activity_camera);
+
+        cameraLayout = findViewById(R.id.layout_camera);
+       // mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
         {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA},
-                    REQUEST_CODE_PERMISSION_READ_CONTACTS);
+                    REQUEST_CODE_PERMISSION_CAMERA);
         }
         else
         {
@@ -39,7 +97,7 @@ public class CameraActivity extends AppCompatActivity
     {
         switch (requestCode)
         {
-            case REQUEST_CODE_PERMISSION_READ_CONTACTS:
+            case REQUEST_CODE_PERMISSION_CAMERA:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 {
                     startCamera();
@@ -53,7 +111,210 @@ public class CameraActivity extends AppCompatActivity
 
     private void startCamera()
     {
-        HolderCallback preview = new HolderCallback(this);
-        setContentView(preview);
+        Toast.makeText(this, "START", Toast.LENGTH_LONG).show();
+
+        surfaceView = new SurfaceView(this);
+        holder = surfaceView.getHolder();
+        holder.addCallback(this);
+        cameraLayout.addView(surfaceView);
     }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder)
+    {
+        Log.d("my_c", "create");
+        try
+        {
+            camera = Camera.open(CAMERA_ID);
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+            camera.autoFocus(this);
+            setCameraDisplayOrientation(CAMERA_ID);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2)
+    {
+        Log.d("my_c", "change");
+        camera.stopPreview();
+        setCameraDisplayOrientation(CAMERA_ID);
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+            camera.autoFocus(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder)
+    {
+        Log.d("my_c", "delete");
+        if (camera != null) {
+            camera.release();
+        }
+    }
+
+    void setCameraDisplayOrientation(int cameraId)
+    {
+        try
+        {
+            // определяем насколько повернут экран от нормального положения
+            int rotation = ((WindowManager) this.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getOrientation();
+            int degrees = 0;
+            switch (rotation) {
+                case Surface.ROTATION_0:
+                    degrees = 0;
+                    break;
+                case Surface.ROTATION_90:
+                    degrees = 90;
+                    break;
+                case Surface.ROTATION_180:
+                    degrees = 180;
+                    break;
+                case Surface.ROTATION_270:
+                    degrees = 270;
+                    break;
+            }
+
+            int result = 0;
+
+            // получаем инфо по камере cameraId
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(cameraId, info);
+
+            result = ((360 - degrees) + info.orientation);
+            result = result % 360;
+
+            camera.setDisplayOrientation(result);
+        }
+        catch (NullPointerException e)
+        {
+            Toast.makeText(this, R.string.error, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onAutoFocus(boolean b, final Camera myCamera)
+    {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.d("my_c", "on focus");
+                if (myCamera != null && (Camera.Parameters.FOCUS_MODE_AUTO.equals(myCamera.getParameters().getFocusMode()) ||
+                        Camera.Parameters.FOCUS_MODE_MACRO.equals(myCamera.getParameters().getFocusMode()))) {
+                    myCamera.autoFocus(CameraActivity.this);
+                    myCamera.takePicture(null, null, null, CameraActivity.this);
+                }
+            }
+        }).start();
+    }
+/*
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private int getRotationCompensation(String cameraId, Activity activity, Context context)
+            throws CameraAccessException {
+        // Get the device's current rotation relative to its "native" orientation.
+        // Then, from the ORIENTATIONS table, look up the angle the image must be
+        // rotated to compensate for the device's rotation.
+        int deviceRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int rotationCompensation = ORIENTATIONS.get(deviceRotation);
+
+        // On most devices, the sensor orientation is 90 degrees, but for some
+        // devices it is 270 degrees. For devices with a sensor orientation of
+        // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
+        CameraManager cameraManager = (CameraManager) context.getSystemService(CAMERA_SERVICE);
+        int sensorOrientation = cameraManager
+                .getCameraCharacteristics(cameraId)
+                .get(CameraCharacteristics.SENSOR_ORIENTATION);
+        rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360;
+
+        // Return the corresponding FirebaseVisionImageMetadata rotation value.
+        int result;
+        switch (rotationCompensation) {
+            case 0:
+                result = FirebaseVisionImageMetadata.ROTATION_0;
+                break;
+            case 90:
+                result = FirebaseVisionImageMetadata.ROTATION_90;
+                break;
+            case 180:
+                result = FirebaseVisionImageMetadata.ROTATION_180;
+                break;
+            case 270:
+                result = FirebaseVisionImageMetadata.ROTATION_270;
+                break;
+            default:
+                result = FirebaseVisionImageMetadata.ROTATION_0;
+                Log.e("my_c", "Bad rotation value: " + rotationCompensation);
+        }
+        return result;
+    }
+*/
+    @Override
+    public void onPictureTaken(byte[] paramArrayOfByte, Camera myCamera)
+    {
+        Log.d("my_c", "on preview");
+        /*DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int rotation = ((WindowManager) this.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getOrientation();
+        Log.d("my_c", "rotation " + String.valueOf(rotation));
+
+        Bitmap imageCamera = BitmapFactory.decodeByteArray(paramArrayOfByte, 0, paramArrayOfByte.length);
+
+        FirebaseVisionImage convertImage = FirebaseVisionImage.fromBitmap(imageCamera);
+        FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+
+        textRecognizer.processImage(convertImage)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText result) {
+                        Toast.makeText(CameraActivity.this, "получилось", Toast.LENGTH_SHORT).show();
+
+                        String resultText = result.getText();
+                        for (FirebaseVisionText.TextBlock block: result.getTextBlocks()) {
+                            String blockText = block.getText();
+                            Float blockConfidence = block.getConfidence();
+                            List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
+                            Point[] blockCornerPoints = block.getCornerPoints();
+                            Rect blockFrame = block.getBoundingBox();
+                            for (FirebaseVisionText.Line line: block.getLines()) {
+                                String lineText = line.getText();
+                                Float lineConfidence = line.getConfidence();
+                                List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
+                                Point[] lineCornerPoints = line.getCornerPoints();
+                                Rect lineFrame = line.getBoundingBox();
+                                for (FirebaseVisionText.Element element: line.getElements()) {
+                                    String elementText = element.getText();
+                                    Float elementConfidence = element.getConfidence();
+                                    List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
+                                    Point[] elementCornerPoints = element.getCornerPoints();
+                                    Rect elementFrame = element.getBoundingBox();
+                                }
+                            }
+                        }
+
+                        Log.d("my_c", resultText);
+                    }
+                })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(CameraActivity.this, "ничего не получилось", Toast.LENGTH_SHORT).show();
+                            }
+                        });*/
+    }
+
 }
