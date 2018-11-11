11c5dacb50130bb7c1a8e39edd757f63112e04d9
Strizhonok <strizhonok15@gmail.com>
2018-11-11 19:58:39 +0300

    new1

--- ConverterMoney/app/src/main/java/com/strizhonok/convertermoney/CameraActivity.java
+++ ConverterMoney/app/src/main/java/com/strizhonok/convertermoney/CameraActivity.java
@@ -1,8 +1,6 @@
 package com.strizhonok.convertermoney;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -10,20 +8,13 @@ import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.hardware.Camera;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
-import android.os.Build;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.util.SparseIntArray;
-import android.view.Display;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -32,6 +23,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -44,7 +36,9 @@ import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 import com.google.firebase.ml.vision.text.RecognizedLanguage;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.List;
 
@@ -57,21 +51,9 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     protected SurfaceView surfaceView;
     protected Camera camera;
     protected LinearLayout cameraLayout;
-   // protected FirebaseAnalytics mFirebaseAnalytics;
-
-   /* private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
-    static
-    {
-        ORIENTATIONS.append(Surface.ROTATION_0, 90);
-        ORIENTATIONS.append(Surface.ROTATION_90, 0);
-        ORIENTATIONS.append(Surface.ROTATION_180, 270);
-        ORIENTATIONS.append(Surface.ROTATION_270, 180);
-    }
-*/
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -81,39 +63,30 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         cameraLayout = findViewById(R.id.layout_camera);
-       // mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-        {
+        //запрос на разрешение работы с камерой
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA},
                     REQUEST_CODE_PERMISSION_CAMERA);
-        }
-        else
-        {
+        } else {
             startCamera();
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-        switch (requestCode)
-        {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
             case REQUEST_CODE_PERMISSION_CAMERA:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     startCamera();
-                }
-                else
-                {
+                } else {
                     Toast.makeText(this, R.string.not_premission, Toast.LENGTH_LONG).show();
                 }
         }
     }
 
-    private void startCamera()
-    {
-        Toast.makeText(this, "START", Toast.LENGTH_LONG).show();
+    private void startCamera() {
+        Log.d("my_c", "start");
 
         surfaceView = new SurfaceView(this);
         holder = surfaceView.getHolder();
@@ -122,11 +95,10 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder)
-    {
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
         Log.d("my_c", "create");
-        try
-        {
+
+        try {
             camera = Camera.open(CAMERA_ID);
             camera.setPreviewDisplay(holder);
             camera.startPreview();
@@ -137,9 +109,9 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2)
-    {
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
         Log.d("my_c", "change");
+
         camera.stopPreview();
         setCameraDisplayOrientation(CAMERA_ID);
         try {
@@ -160,13 +132,11 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
         }
     }
 
-    void setCameraDisplayOrientation(int cameraId)
-    {
+    void setCameraDisplayOrientation(int cameraId) {
         // определяем насколько повернут экран от нормального положения
         int rotation = getWindowManager().getDefaultDisplay().getRotation();
         int degrees = 0;
-        switch (rotation) //потом пригодится, пока всегда 0
-        {
+        switch (rotation) { //потом пригодится, пока всегда 0
             case Surface.ROTATION_0:
                 degrees = 0;
                 break;
@@ -180,97 +150,55 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
                 degrees = 270;
                 break;
         }
-
-        int result = 0;
-
         // получаем инфо по камере cameraId
         Camera.CameraInfo info = new Camera.CameraInfo();
         Camera.getCameraInfo(cameraId, info);
 
+        int result = 0;
         result = ((360 - degrees) + info.orientation);
         result = result % 360;
 
         camera.setDisplayOrientation(result);
     }
 
-    public void clickCameraImage(View view)
-    {
+    public void clickCameraImage(View view) {
         Log.d("my_c", "on click");
-        //camera.autoFocus(this);
+
+        camera.autoFocus(this);
     }
 
     @Override
-    public void onAutoFocus(boolean b, final Camera myCamera)
-    {
+    public void onAutoFocus(boolean b, final Camera myCamera) {
         Log.d("my_c", "on focus");
+
         if (myCamera != null && (Camera.Parameters.FOCUS_MODE_AUTO.equals(myCamera.getParameters().getFocusMode()) ||
                 Camera.Parameters.FOCUS_MODE_MACRO.equals(myCamera.getParameters().getFocusMode())))
         {
             myCamera.autoFocus(CameraActivity.this);
             myCamera.takePicture(null, null, null, CameraActivity.this);
+            myCamera.cancelAutoFocus();
         }
     }
-/*
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private int getRotationCompensation(String cameraId, Activity activity, Context context)
-            throws CameraAccessException {
-        // Get the device's current rotation relative to its "native" orientation.
-        // Then, from the ORIENTATIONS table, look up the angle the image must be
-        // rotated to compensate for the device's rotation.
-        int deviceRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
-        int rotationCompensation = ORIENTATIONS.get(deviceRotation);
 
-        // On most devices, the sensor orientation is 90 degrees, but for some
-        // devices it is 270 degrees. For devices with a sensor orientation of
-        // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
-        CameraManager cameraManager = (CameraManager) context.getSystemService(CAMERA_SERVICE);
-        int sensorOrientation = cameraManager
-                .getCameraCharacteristics(cameraId)
-                .get(CameraCharacteristics.SENSOR_ORIENTATION);
-        rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360;
-
-        // Return the corresponding FirebaseVisionImageMetadata rotation value.
-        int result;
-        switch (rotationCompensation) {
-            case 0:
-                result = FirebaseVisionImageMetadata.ROTATION_0;
-                break;
-            case 90:
-                result = FirebaseVisionImageMetadata.ROTATION_90;
-                break;
-            case 180:
-                result = FirebaseVisionImageMetadata.ROTATION_180;
-                break;
-            case 270:
-                result = FirebaseVisionImageMetadata.ROTATION_270;
-                break;
-            default:
-                result = FirebaseVisionImageMetadata.ROTATION_0;
-                Log.e("my_c", "Bad rotation value: " + rotationCompensation);
-        }
-        return result;
+    private Bitmap rotateImage(Bitmap source, float angle){
+        Bitmap rotateBitmap;
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+       // matrix.postScale(source.getWidth(), source.getHeight());
+        rotateBitmap = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+        return rotateBitmap;
     }
-*/
+
     @Override
-    public void onPictureTaken(byte[] paramArrayOfByte, Camera myCamera)
-    {
-        try
-        {
-            Log.d("my_c", "on preview");
-        }
-        catch( RuntimeException e)
-        {
-            Log.d("my_c", "error" + e.toString());
-        }
-        /*DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-
-        int rotation = ((WindowManager) this.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getOrientation();
-        Log.d("my_c", "rotation " + String.valueOf(rotation));
+    public void onPictureTaken(byte[] paramArrayOfByte, Camera myCamera) {
+        Log.d("my_c", "on preview");
 
         Bitmap imageCamera = BitmapFactory.decodeByteArray(paramArrayOfByte, 0, paramArrayOfByte.length);
 
-        FirebaseVisionImage convertImage = FirebaseVisionImage.fromBitmap(imageCamera);
+       // ImageView iv = findViewById(R.id.imageView);
+      //  iv.setImageBitmap(rotateImage(imageCamera, 90));
+
+        FirebaseVisionImage convertImage = FirebaseVisionImage.fromBitmap(imageCamera);//rotateImage(imageCamera, 180));
         FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
                 .getOnDeviceTextRecognizer();
 
@@ -303,7 +231,7 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
                             }
                         }
 
-                        Log.d("my_c", resultText);
+                        Log.d("my_c", "result " + resultText);
                     }
                 })
                 .addOnFailureListener(
@@ -312,6 +240,6 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
                             public void onFailure(@NonNull Exception e) {
                                 Toast.makeText(CameraActivity.this, "ничего не получилось", Toast.LENGTH_SHORT).show();
                             }
-                        });*/
+                        });
     }
 }
